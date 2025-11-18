57b00006875aaa0d3ce67274347704e27ec101f5
Madhav Sharma <158333993+Madhav-Sharma-07@users.noreply.github.com>
2025-11-18 03:32:07 -0500

    Update test_queries.sql

--- sql/test_queries.sql
+++ sql/test_queries.sql
@@ -1,3 +1,4 @@
+SELECT symbol, COUNT(*) FROM klines GROUP BY symbol;
 SELECT symbol, COUNT(*) FROM funding GROUP BY symbol;
 SELECT symbol, COUNT(*) FROM open_interest GROUP BY symbol;
 SELECT symbol, COUNT(*) FROM premium_index GROUP BY symbol;
